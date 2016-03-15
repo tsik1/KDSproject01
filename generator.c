f5e2a361372a46adc4ea96131322a9725c2e8b19
Aristeidis Noulis <arisnoul@csd.auth.gr>
2016-03-15 22:09:05 +0200

    Update for the legibility of the code

--- generator.c
+++ generator.c
@@ -11,15 +11,17 @@ void saveFile(char* filename, float* cords[3],int n)
         return;
     }
     int i;
-     for(i=0;i<n;i++){
+     for(i=0;i<n;i++)
+     {
          fprintf(fp,"%f %f %f\n",cords[i][0],cords[i][1],cords[i][2]);
      }
     fclose(fp);
 }
 
-
-int main(int argc, char *argv[]) {
-    if (argc<1 || argc>3) {
+int main(int argc, char *argv[]) 
+{
+    if (argc<1 || argc>3) 
+    {
       printf("Error");
       return;
     }
@@ -31,13 +33,15 @@ int main(int argc, char *argv[]) {
     int i,j;
     float **cords;
     cords=calloc(coordinate_index,sizeof(float*));
-    for(j=0;j<coordinate_index;j++) {
+    for(j=0;j<coordinate_index;j++) 
+    {
       cords[j]=calloc(3,sizeof(float));
     }
     ltime = time(NULL);
     utime = (unsigned int) ltime/2;
     srand(utime);
-    for(i=0;i<coordinate_index;i++){
+    for(i=0;i<coordinate_index;i++)
+    {
       cords[i][0]=(float)34*rand()/(RAND_MAX-1);
       cords[i][1]=(float)34*rand()/(RAND_MAX-1);
       cords[i][2]=(float)34*rand()/(RAND_MAX-1);
